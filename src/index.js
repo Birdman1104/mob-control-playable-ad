95fd1820421f99bfba12ee0e20adbe71555249dd
Birdman1104 <arsenmazmanyan11@gmail.com>
2024-11-27 21:41:32 +0400

    Added assets.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
 import * as THREE from "three";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-import model from "./assets/model.glb";
-import sound from "./assets/sound.mp3";
-import texture from "./assets/texture.jpg";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -10,33 +6,33 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const loader = new GLTFLoader();
-loader.load(model, (gltf) => scene.add(gltf.scene));
+// const loader = new GLTFLoader();
+// loader.load(model, (gltf) => scene.add(gltf.scene));
 
-const textureLoader = new THREE.TextureLoader();
-const material = new THREE.MeshBasicMaterial({ map: textureLoader.load(texture) });
-const geometry = new THREE.BoxGeometry();
-const cube = new THREE.Mesh(geometry, material);
-cube.position.y = -1;
-scene.add(cube);
+// const textureLoader = new THREE.TextureLoader();
+// const material = new THREE.MeshBasicMaterial({ map: textureLoader.load(texture) });
+// const geometry = new THREE.BoxGeometry();
+// const cube = new THREE.Mesh(geometry, material);
+// cube.position.y = -1;
+// scene.add(cube);
 
 const listener = new THREE.AudioListener();
 camera.add(listener);
-const soundSource = new THREE.Audio(listener);
-const audioLoader = new THREE.AudioLoader();
-audioLoader.load(sound, (buffer) => {
-  soundSource.setBuffer(buffer);
-  soundSource.setLoop(false);
-  soundSource.play();
-});
+// const soundSource = new THREE.Audio(listener);
+// const audioLoader = new THREE.AudioLoader();
+// audioLoader.load(sound, (buffer) => {
+//   soundSource.setBuffer(buffer);
+//   soundSource.setLoop(false);
+//   soundSource.play();
+// });
 
 camera.position.z = 1;
-const animate = () => {
-  requestAnimationFrame(animate);
-  cube.rotation.x += 0.02;
-  cube.rotation.y += 0.01;
-  renderer.render(scene, camera);
-};
+// const animate = () => {
+//   requestAnimationFrame(animate);
+//   cube.rotation.x += 0.02;
+//   cube.rotation.y += 0.01;
+//   renderer.render(scene, camera);
+// };
 
-animate();
+// animate();
 
